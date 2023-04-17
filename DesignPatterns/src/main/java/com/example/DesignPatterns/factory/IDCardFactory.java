0f74951ff72640a5bbc2141004eabbd9089bfd39
陈梦杰 <chen_mengj@outlook.com>
2023-04-17 10:38:01 +0800

    工厂模式

--- DesignPatterns/src/main/java/com/example/DesignPatterns/factory/IDCardFactory.java
+++ DesignPatterns/src/main/java/com/example/DesignPatterns/factory/IDCardFactory.java
@@ -1,27 +1,26 @@
 package com.example.DesignPatterns.factory;
 
-import javax.jws.Oneway;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * @author admin
  */
-public class IDCardFactory extends Factory{
+public class IDCardFactory extends Factory {
 
     List<String> owners = new CopyOnWriteArrayList<>();
+
     @Override
     public Product createProduct(String owner) {
-        System.out.println("为"+owner+"创建产品");
+        System.out.println("为" + owner + "创建产品");
         return new IDCardProduct(owner);
     }
 
     @Override
     public void registerProduct(Product p) {
-        String owner = ((IDCardProduct)p).getOwner();
+        String owner = ((IDCardProduct) p).getOwner();
         owners.add(owner);
-        System.out.println("为"+owner+"注册产品");
+        System.out.println("为" + owner + "注册产品");
     }
 
     @Override
